ee008a4a58342f2ec9e857a95a2c7c1923e79688
DinhMinh2810 <dinhminh28102000@gmail.com>
2021-09-17 19:39:18 +0700

    “Message”

--- app/src/main/java/org/meicode/asm_form/MainActivity.java
+++ app/src/main/java/org/meicode/asm_form/MainActivity.java
@@ -9,6 +9,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.InputType;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -16,6 +18,7 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
@@ -27,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
     // four text fields
     EditText etUserName, etPrice, etNotes, date_time_in;
     RadioGroup radioGroup, radioBed;
+    TextView disablePastDate, disableTime;
 
     // one boolean variable to check whether all the text fields
     // are filled by the user, properly or not.
@@ -51,16 +55,67 @@ public class MainActivity extends AppCompatActivity {
         radioGroup = (RadioGroup) findViewById(R.id.rbGroup);
         radioBed = (RadioGroup) findViewById(R.id.rbBed);
 
-        date_time_in = findViewById(R.id.date_time_input);
-        date_time_in.setInputType(InputType.TYPE_NULL);
+//        date_time_in = findViewById(R.id.date_time_input);
+//        date_time_in.setInputType(InputType.TYPE_NULL);
 
-        date_time_in.setOnClickListener(new View.OnClickListener() {
+//        date_time_in.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                showDateTimeDialog(date_time_in);
+//
+//            }
+//        });
+
+        //time
+        disablePastDate = findViewById(R.id.disable_past_date);
+        disablePastDate.setInputType(InputType.TYPE_NULL);
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        disableTime = findViewById(R.id.disabletime);
+        disableTime.setInputType(InputType.TYPE_NULL);
+
+        disablePastDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showDateTimeDialog(date_time_in);
-
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayofMonth) {
+//                        String sDate = dayofMonth + "/" + month + '/' + year;
+                        CharSequence sDate = DateFormat.format("EEE, d MMM yyyy", calendar);
+                        disablePastDate.setText(sDate);
+                    }
+                }, year, month, day);
+                datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+                datePickerDialog.show();
             }
         });
+
+        disableTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int HOUR = calendar.get(Calendar.HOUR);
+                int MINUTE = calendar.get(Calendar.MINUTE);
+                boolean is24HourFormat = DateFormat.is24HourFormat(MainActivity.this);
+
+                TimePickerDialog timePickerDialog = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+                        Calendar calendar1 = Calendar.getInstance();
+                        calendar1.set(Calendar.HOUR, hour);
+                        calendar1.set(Calendar.MINUTE, minute);
+                        CharSequence charSequence = DateFormat.format("hh:mm a", calendar1);
+                        disableTime.setText(charSequence);
+                    }
+                }, HOUR, MINUTE, is24HourFormat);
+
+                timePickerDialog.show();
+            }
+        });
+
 
         // handle the PROCEED button
         bProceed.setOnClickListener(new View.OnClickListener() {
@@ -80,30 +135,30 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void showDateTimeDialog(final EditText date_time_in) {
-        final Calendar calendar = Calendar.getInstance();
-        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                calendar.set(Calendar.YEAR, year);
-                calendar.set(Calendar.MONTH, month);
-                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-
-
-                TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
-                        calendar.set(Calendar.MINUTE, minute);
-                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yy-MM-dd HH:mm");
-                        date_time_in.setText(simpleDateFormat.format(calendar.getTime()));
-                    }
-                };
-                new TimePickerDialog(MainActivity.this, timeSetListener, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false).show();
-            }
-        };
-        new DatePickerDialog(MainActivity.this, dateSetListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
-    }
+//    private void showDateTimeDialog(final EditText date_time_in) {
+//        final Calendar calendar = Calendar.getInstance();
+//        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+//            @Override
+//            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+//                calendar.set(Calendar.YEAR, year);
+//                calendar.set(Calendar.MONTH, month);
+//                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+//
+//
+//                TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+//                    @Override
+//                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+//                        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+//                        calendar.set(Calendar.MINUTE, minute);
+//                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yy-MM-dd HH:mm");
+//                        date_time_in.setText(simpleDateFormat.format(calendar.getTime()));
+//                    }
+//                };
+//                new TimePickerDialog(MainActivity.this, timeSetListener, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false).show();
+//            }
+//        };
+//        new DatePickerDialog(MainActivity.this, dateSetListener, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+//    }
 
 
     // function which checks all the text fields
@@ -114,7 +169,7 @@ public class MainActivity extends AppCompatActivity {
         int isSelected = radioGroup.getCheckedRadioButtonId();
         int isSelectedBed = radioBed.getCheckedRadioButtonId();
 
-        if (isSelected == -1 && isSelectedBed == -1 && date_time_in.length() == 0 && etPrice.length() == 0 && etUserName.length() == 0) {
+        if (isSelected == -1 && isSelectedBed == -1 && disablePastDate.length() == 0 && disableTime.length() == 0 && etPrice.length() == 0 && etUserName.length() == 0) {
             Toast.makeText(MainActivity.this, Html.fromHtml("<font color='red' ><big>" + "Please fill all required field" + "</big></font>"), Toast.LENGTH_LONG).show();
             return false;
         }
@@ -128,14 +183,28 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(MainActivity.this, Html.fromHtml("<font color='red' ><big>" + "Please select opinions BedRooms field" + "</big></font>"), Toast.LENGTH_LONG).show();
             return false;
         }
+//        if (!disablePastDate.getText().toString().matches("")) {
+//            Toast.makeText(MainActivity.this, Html.fromHtml("<font color='red' ><big>" + "Please Date Time field" + "</big></font>"), Toast.LENGTH_LONG).show();
+//            return false;
+//        }
 
-        if (date_time_in.length() == 0) {
-            Toast.makeText(MainActivity.this, Html.fromHtml("<font color='red' ><big>" + "Please Date Time field" + "</big></font>"), Toast.LENGTH_LONG).show();
+        if (disablePastDate.length() == 0) {
+            Toast.makeText(MainActivity.this, Html.fromHtml("<font color='red' ><big>" + "Field Date is required" + "</big></font>"), Toast.LENGTH_LONG).show();
             return false;
         }
 
+        if (disableTime.length() == 0) {
+            Toast.makeText(MainActivity.this, Html.fromHtml("<font color='red' ><big>" + "Field Time is required" + "</big></font>"), Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+//        if (date_time_in.length() == 0) {
+//            Toast.makeText(MainActivity.this, Html.fromHtml("<font color='red' ><big>" + "Please Date Time field" + "</big></font>"), Toast.LENGTH_LONG).show();
+//            return false;
+//        }
+
         if (etPrice.length() == 0) {
-            etPrice.setError("Monthly rent price is required");
+            etPrice.setError("Field Monthly rent price is required");
             return false;
         }
 
@@ -149,7 +218,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if (etUserName.length() == 0) {
-            etUserName.setError("User Name field is required");
+            etUserName.setError("Field User Name field is required");
             return false;
         }
 
